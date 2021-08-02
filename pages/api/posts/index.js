147549b007b7aac34b54a1cbef6cbfd1c78d7812
Nicole Reed <nicolereed@Nicoles-MacBook-Air.local>
2021-08-02 15:23:21 +0200

    get all posts/get by topicId sorted by date

---
+++ pages/api/posts/index.js
@@ -0,0 +1,21 @@
+import { Record, String, Optional, Union, Literal, ValidationError } from 'runtypes'
+import { Post } from '../../../models/post'
+import connectDB from '../../../middleware/mongodb'
+
+const handler = async (req, res) => {
+    if (req.method === 'GET') {
+        try {
+            const posts = await Post.find({}).sort({ _id: -1 }) //sorts by date newest to oldest
+
+            res.send(posts)
+        } catch (error) {
+            console.log(error)
+            res.status(500).send(error.message)
+        }
+
+    } else {
+        res.status(400).send(`no endpoint ${req.method} /posts`)
+    }
+}
+
+export default connectDB(handler)

2e49c0481963dcb68247966ff70d1cb2cc5ef368
Nicole Reed <nicolereed@nicoles-air.home>
2021-08-27 16:15:00 +0200

    pagination on posts in search function

--- pages/search.js
+++ pages/search.js
@@ -5,18 +5,19 @@ import React from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-export default function Topic() {
+export default function Search() {
     const [posts, setPosts] = useState([])
     const [keyword, setKeyword] = useState('')
+    const [pageNumber, setPageNumber] = useState(1)
 
     const onKeywordChange = (event) => {
         setKeyword(event.target.value)
     }
 
-    const getPosts = async event => {
+    const getPosts = async (pageNumber, event) => {
         try {
-            event.preventDefault()
-            const res = await axios.get(`/api/search`, { params: { keyword } })
+            event && event.preventDefault()
+            const res = await axios.get(`/api/search`, { params: { page: pageNumber, keyword } })
 
             setPosts(res.data.posts)
         } catch (error) {
@@ -24,8 +25,38 @@ export default function Topic() {
         }
     }
     useEffect(() => {
-        getPosts()
+
     }, [])
+
+    const onClickNext = () => {
+        try {
+            const newPageNumber = pageNumber + 1
+            getPosts(newPageNumber)
+            setPageNumber(newPageNumber)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    const onClickBack = () => {
+        try {
+            const newPageNumber = pageNumber - 1
+            getPosts(newPageNumber)
+            setPageNumber(newPageNumber)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    const onSubmit = async (event) => {
+        try {
+            event.preventDefault()
+
+            getPosts(pageNumber, event)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
 
     return (
         <div >
@@ -33,12 +64,15 @@ export default function Topic() {
                 <title>Search</title>
             </Head>
             <Layout>
-                <form onSubmit={getPosts}>
-                    {/* <label htmlFor="name">Search: </label> */}
+                <form onSubmit={onSubmit}>
                     <input id='keyword' name='keyword' type="text" placeholder='keyword' value={keyword} onChange={onKeywordChange} required />
                     <button type="submit"> Search </button>
                 </form>
                 {posts.length > 0 ? <Posts posts={posts} /> : <h2>No Posts Matching Keyword</h2>}
+                {pageNumber > 1 && <button onClick={onClickBack}>Back</button>}
+
+                {posts.length === 10 && <button onClick={onClickNext}>Next</button>}
+
             </Layout>
         </div>
     )
